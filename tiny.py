c28742448eafea9ad5e86e5899d815c4e6e0b102
linh <fastrocket@gmail.com>
2023-10-28 17:06:32 -0600

    Stuff about gfpgan, untested for reference

--- tiny.py
+++ tiny.py
@@ -14,3 +14,29 @@ prompt = "an apple, 4k"
 image = pipe(prompt).images[0]  
     
 image.save("apple.png")
+
+
+# gfpgan
+
+import torch
+from gfpgan import GFPGANer
+
+# https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth
+# Load the model
+model = GFPGANer(
+    model_path='path/to/model.pth',
+    upscale=4,
+    channel_multiplier=2,
+    pre_pad=True,
+    half=True,
+    device=torch.device('cuda')
+)
+
+# Load an image
+img = Image.open('path/to/image.png')
+
+# Restore the image
+restored_img = model.enhance(img)
+
+# Save the restored image
+restored_img.save('path/to/restored_image.png')
